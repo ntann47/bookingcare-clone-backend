a185838ce9ad18ae86a6ae84bb0e0534df46cced
Truong An Ngo <ntann47@gmail.com>
2022-09-16 21:20:53 +0700

    3rd commit

--- src/server.js
+++ src/server.js
@@ -8,12 +8,20 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-app.use(cors({ credentials: true, origin: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-credentials", "true");
+  res.header("Acces-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Acces-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token"
+  );
+  res.header(
+    "Acces-Control-Allow-Methods",
+    "GET,OPTIONS, POST, DELETE, PUT, PATCH"
+  );
+
+  next();
+});
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
